57727291dff8ed9975926a3a96cfdccda19f598b
competitionlabs <admin@competitionlabs.com>
2018-03-21 13:26:39 +0000

    updated transformer contract

--- src/main/scala/com/competitionlabs/transformers/CLMQTransformer.scala
+++ src/main/scala/com/competitionlabs/transformers/CLMQTransformer.scala
@@ -7,6 +7,13 @@
 package com.competitionlabs.transformers
 
 trait CLMQTransformer {
+
+	/**
+	  * This method gets executed when a message is received on the message queue
+	  * @param message The message
+	  * @param competitionLabsApi The CompetitionLabs API
+	  */
+	def apply(message: Array[Byte], competitionLabsApi: CompetitionLabsApi, args: Map[String,Any]): Unit
 	
 	/**
 	  * This method gets executed when a message is received on the message queue
@@ -15,7 +22,15 @@ trait CLMQTransformer {
 	  * @param exchangeName The exchange name for the incoming message from the Envelope of AMQP client
 	  * @param competitionLabsApi The CompetitionLabs API
 	  */
-	def rabbit(message: Array[Byte], routingKey: String, exchangeName: String, competitionLabsApi: CompetitionLabsApi): Unit = {}
+	def rabbit(message: Array[Byte], routingKey: String, exchangeName: String, competitionLabsApi: CompetitionLabsApi): Unit = {
+		apply(
+			message,
+			competitionLabsApi,
+			Map(
+				"routingKey" -> routingKey,
+				"exchangeName" -> exchangeName
+			))
+	}
 
 	/**
 	  * This method gets executed when a message is received on the message queue
@@ -23,5 +38,12 @@ trait CLMQTransformer {
 	  * @param key The key for the incoming message from the Kafka broker
 	  * @param competitionLabsApi The CompetitionLabs API
 	  */
-	def kafka(key: Array[Byte], message: Array[Byte], competitionLabsApi: CompetitionLabsApi): Unit = {}
+	def kafka(key: Array[Byte], message: Array[Byte], competitionLabsApi: CompetitionLabsApi): Unit = {
+		apply(
+			message,
+			competitionLabsApi,
+			Map(
+				"key" -> key,
+			))
+	}
 }

ec427fcecae22b4eb5486e53746cb7647c4a5714
competitionlabs <admin@competitionlabs.com>
2018-05-22 14:28:49 +0100

    Added sub accounts

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ name := "competitionlabs-transformers"
 
 organization := "com.competitionlabs"
 
-version := "1.6"
+version := "1.7"
 
 isSnapshot := true
 
---
+++ src/main/scala/com/competitionlabs/transformers/domain/BasicContestModel.scala
@@ -0,0 +1,78 @@
+/***
+  *  Copyright (C) Competition Labs Ltd, Inc - All Rights Reserved
+  *  Unauthorized copying of this file, via any medium is strictly prohibited
+  *  Proprietary and confidential
+  *  Written by Competition Labs Ltd, 2017
+  */
+
+package com.competitionlabs.transformers.domain
+
+import org.joda.time.DateTime
+
+abstract class BasicContestModel {
+
+	/**
+	  *
+	  * @return Get the name of Contest
+	  */
+	def getName: String
+
+	/**
+	  *
+	  * @return Get the description of Contest
+	  */
+	def getCompetitionId: String
+
+	/**
+	  *
+	  * @return Get start date of Contest
+	  */
+	def getStartDate: DateTime
+
+	/**
+	  *
+	  * @return Get end date of Contest
+	  */
+	def getEndDate: Option[DateTime]
+
+	/**
+	  *
+	  * @return Get created time of Contest
+	  */
+	def getCreatedTime: DateTime
+
+	/**
+	  *
+	  * @return CL id for Contest
+	  */
+	def getClContestId: String
+
+	/**
+	  *
+	  * @return Product reference ids used in rules for this Contest
+	  */
+	def getProductRefIds: Option[Array[String]]
+
+	/**
+	  *
+	  * @return Returns the status of Contest
+	  */
+	def getStatus: String
+
+	/**
+	  *
+	  * @return Key value pair of metadata information
+	  */
+	def getMetaData: Option[Map[String, String]]
+
+	/**
+	  * Update metadata of the Contest
+	  * @param metadata Key value pair of information
+	  */
+	def setMetaData(metadata: Map[String, String]): Unit
+
+	/**
+	  * System will update the status
+	  */
+	def setStatus(): Unit
+}

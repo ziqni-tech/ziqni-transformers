a6570600fd362bb1a230dce55eee88234554c23e
rahul-patadia <ronly2008@gmail.com>
2019-11-06 22:30:17 +0000

    added new method

--- src/main/scala/com/competitionlabs/transformers/CompetitionLabsApi.scala
+++ src/main/scala/com/competitionlabs/transformers/CompetitionLabsApi.scala
@@ -7,17 +7,17 @@
 package com.competitionlabs.transformers
 
 import java.nio.charset.Charset
-
 import com.competitionlabs.transformers.domain._
 import org.joda.time.DateTime
 import org.json4s.JsonAST.JValue
 
 trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
-	/***
+	/** *
 	  * Generate a unique time based UUID, this can be used to set the batchId value if
 	  * a single event is transformed into multiple distinct events (facts) and a correlation
 	  * needs to be maintained
+	  *
 	  * @return A time based UUID as a string
 	  */
 	def nextId: String
@@ -37,8 +37,11 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 	  */
 	def subAccounts: Map[String, CompetitionLabsApi]
 
+	def getSubAccount(spaceName: String): Option[CompetitionLabsApi]
+
 	/**
 	  * Insert an event into your CompetitionLabs space
+	  *
 	  * @param event The event to add
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
@@ -46,6 +49,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Insert a sequence of events into your CompetitionLabs space
+	  *
 	  * @param events The events to add
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
@@ -53,22 +57,25 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Insert an event into your CompetitionLabs space
+	  *
 	  * @param event The event to add
 	  * @param delay The time in milliseconds to delay processing of event
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	def pushEventWithDelay(event: BasicEventModel, delay:Long): Boolean
+	def pushEventWithDelay(event: BasicEventModel, delay: Long): Boolean
 
 	/**
 	  * Insert a sequence of events into your CompetitionLabs space
+	  *
 	  * @param events The events to add
-	  * @param delay The time in milliseconds to delay processing of event
+	  * @param delay  The time in milliseconds to delay processing of event
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	def pushEventsWithDelay(events: Seq[BasicEventModel], delay:Long): Boolean
+	def pushEventsWithDelay(events: Seq[BasicEventModel], delay: Long): Boolean
 
 	/**
 	  * Get the CompetitionLabs id for the member based on your reference id
+	  *
 	  * @param memberReferenceId The id used to identify this member in the sending system
 	  * @return The id used in the CompetitionLabs system or None if the user does not exist
 	  */
@@ -76,6 +83,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Get the member reference id for the member based on CompetitionLabs id
+	  *
 	  * @param memberId The id used to identify this member in the sending system
 	  * @return The id used in the CompetitionLabs system or None if the user does not exist
 	  */
@@ -83,21 +91,22 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Create a member in the CompetitionLabs system
+	  *
 	  * @param memberReferenceId The id used to identify this member in the sending system
-	  * @param displayName Display name
-	  * @param groups The groups to add this member to
+	  * @param displayName       Display name
+	  * @param groups            The groups to add this member to
 	  * @return The id used in the CompetitionLabs system
 	  */
-	def createMember(memberReferenceId: String, displayName: String, groups:Seq[String], metaData: Option[Map[String, String]] = None): Option[String]
+	def createMember(memberReferenceId: String, displayName: String, groups: Seq[String], metaData: Option[Map[String, String]] = None): Option[String]
 
 	/**
 	  *
-	  * @param clMemberId CL Member Id
-	  * @param displayName Display name
+	  * @param clMemberId     CL Member Id
+	  * @param displayName    Display name
 	  * @param groupsToUpdate The groups to add this member to
 	  * @return The id used in the CompetitionLabs system
 	  */
-	def updateMember(clMemberId: String, memberReferenceId: Option[String], displayName: Option[String], groupsToUpdate:Option[Array[String]], metaData: Option[Map[String, String]]): Option[String]
+	def updateMember(clMemberId: String, memberReferenceId: Option[String], displayName: Option[String], groupsToUpdate: Option[Array[String]], metaData: Option[Map[String, String]]): Option[String]
 
 	/**
 	  *
@@ -108,6 +117,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Get the CompetitionLabs id for the product based on your reference id
+	  *
 	  * @param productReferenceId The id used to identify this product in the sending system
 	  * @return The id used in the CompetitionLabs system or None if the product does not exist
 	  */
@@ -115,6 +125,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Get the product id for the product based on your CompetitionLabs id
+	  *
 	  * @param productId The id used to identify this product in the sending system
 	  * @return The id used in the CompetitionLabs system or None if the product does not exist
 	  */
@@ -122,25 +133,25 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  *
-	  * @param productReferenceId The id used to identify this product in the sending system
-	  * @param displayName Display name
-	  * @param providers The providers of this product
-	  * @param productType The type of product
+	  * @param productReferenceId      The id used to identify this product in the sending system
+	  * @param displayName             Display name
+	  * @param providers               The providers of this product
+	  * @param productType             The type of product
 	  * @param defaultAdjustmentFactor The default adjustment factor to apply
 	  * @return The id used in the CompetitionLabs system
 	  */
-	def createProduct(productReferenceId: String, displayName: String, providers:Seq[String], productType: String, defaultAdjustmentFactor: Double, metaData: Option[Map[String, String]] = None): Option[String]
+	def createProduct(productReferenceId: String, displayName: String, providers: Seq[String], productType: String, defaultAdjustmentFactor: Double, metaData: Option[Map[String, String]] = None): Option[String]
 
 	/**
 	  *
-	  * @param clProductId CL Product Id
-	  * @param displayName Display name
-	  * @param providers The providers of this product
-	  * @param productType The type of product
+	  * @param clProductId             CL Product Id
+	  * @param displayName             Display name
+	  * @param providers               The providers of this product
+	  * @param productType             The type of product
 	  * @param defaultAdjustmentFactor The default adjustment factor to apply
 	  * @return The id used in the CompetitionLabs system
 	  */
-	def updateProduct(clProductId: String, productReferenceId: Option[String], displayName: Option[String], providers:Option[Array[String]], productType: Option[String], defaultAdjustmentFactor: Option[Double], metaData: Option[Map[String, String]]): Option[String]
+	def updateProduct(clProductId: String, productReferenceId: Option[String], displayName: Option[String], providers: Option[Array[String]], productType: Option[String], defaultAdjustmentFactor: Option[Double], metaData: Option[Map[String, String]]): Option[String]
 
 	/**
 	  *
@@ -151,30 +162,34 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Verify if the event action type exists in your space
+	  *
 	  * @param action The action
 	  * @return True of the action was created
 	  */
 	def eventActionExists(action: String): Boolean
 
-	/***
+	/** *
 	  * Create the action in your space
+	  *
 	  * @param action True on success false on failure
 	  * @return
 	  */
 	def createEventAction(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None): Boolean
 
-	/***
+	/** *
 	  * Update the action in your space
+	  *
 	  * @param action True on success false on failure
 	  * @return
 	  */
-	def updateEventAction(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None, unitOfMeasureType: Option[String]= None): Boolean
-	
+	def updateEventAction(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None, unitOfMeasureType: Option[String] = None): Boolean
+
 	/**
 	  * [PROTOTYPE]
 	  * Get the spot rate for a currency at a point in time from Oanda
+	  *
 	  * @param fromCurrency From currency ISO3 code
-	  * @param toCurrency To currency ISO3 code
+	  * @param toCurrency   To currency ISO3 code
 	  * @return The rate
 	  */
 	def spotExchangeRate(fromCurrency: String, toCurrency: String, pointInTime: DateTime): Double
@@ -223,6 +238,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Converts a json string to a JValue
+	  *
 	  * @param body The string to deserialise
 	  * @return JValue or throws exception
 	  */
@@ -230,6 +246,7 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Converts a map to a json string
+	  *
 	  * @param obj The object to serialise
 	  * @return json string or throws exception
 	  */
@@ -237,7 +254,8 @@ trait CompetitionLabsApi extends CompetitionLabsApiHttp {
 
 	/**
 	  * Converts byte array to String using UTF-8
-	  * @param body The string encoded as bytes
+	  *
+	  * @param body    The string encoded as bytes
 	  * @param charset Optional, character set to decode byte array, default is UTF-8
 	  * @return Decoded string or throws exception
 	  */

ee9e396f270b256605a9b3469e6f54b000dcf98e
vossie <carel.vosloo@bronzegate.com>
2022-09-24 01:08:41 +0100

    Moving to a brighter futures

--- src/main/scala/com/ziqni/transformers/ZiqniApiAsync.scala
+++ src/main/scala/com/ziqni/transformers/ZiqniApiAsync.scala
@@ -6,14 +6,12 @@
   */
 package com.ziqni.transformers
 
-import com.ziqni.transformers.domain.{BasicAchievementModel, BasicAwardModel, BasicContestModel, BasicEventModel, BasicMemberModel, BasicProductModel, BasicRewardModel, BasicUnitOfMeasureModel}
+import com.ziqni.transformers.domain._
 
 import java.nio.charset.Charset
-import com.ziqni.transformers.domain._
-import org.joda.time.DateTime
-import org.json4s.JsonAST.JValue
+import scala.concurrent.Future
 
-trait ZiqniApi extends ZiqniApiAsync {
+trait ZiqniApiAsync extends ZiqniApiHttp {
 
 	/** *
 	  * Generate a unique time based UUID, this can be used to set the batchId value if
@@ -22,7 +20,6 @@ trait ZiqniApi extends ZiqniApiAsync {
 	  *
 	  * @return A time based UUID as a string
 	  */
-
 	def nextId: String
 
 	/**
@@ -38,74 +35,61 @@ trait ZiqniApi extends ZiqniApiAsync {
 	/**
 	  * Get sub accounts for this master account if any exists
 	  */
-	def subAccounts: Map[String, ZiqniApi]
+	def subAccounts: Map[String, ZiqniApiAsync]
 
-	def getSubAccount(spaceName: String): Option[ZiqniApi]
+	def getSubAccount(spaceName: String): Option[ZiqniApiAsync]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Insert an event into your Ziqni space
 	  *
 	  * @param event The event to add
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	@Deprecated(since = "v1.0.6")
-	def pushEvent(event: BasicEventModel): Boolean
+	def pushEventAsync(event: BasicEventModel): Future[Boolean]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Insert a sequence of events into your Ziqni space
 	  *
 	  * @param events The events to add
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	@Deprecated(since = "v1.0.6")
-	def pushEvents(events: Seq[BasicEventModel]): Boolean
+	def pushEventsAsync(events: Seq[BasicEventModel]): Future[Boolean]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Insert an event into your Ziqni space
 	  *
 	  * @param event The event to add
 	  * @param delay The time in milliseconds to delay processing of event
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	@Deprecated(since = "v1.0.6")
-	def pushEventWithDelay(event: BasicEventModel, delay: Long): Boolean
+	def pushEventWithDelayAsync(event: BasicEventModel, delay: Long): Future[Boolean]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Insert a sequence of events into your Ziqni space
 	  *
 	  * @param events The events to add
 	  * @param delay  The time in milliseconds to delay processing of event
 	  * @return True on success, false on duplicate and exception if malformed
 	  */
-	@Deprecated(since = "v1.0.6")
-	def pushEventsWithDelay(events: Seq[BasicEventModel], delay: Long): Boolean
+	def pushEventsWithDelayAsync(events: Seq[BasicEventModel], delay: Long): Future[Boolean]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Get the Ziqni id for the member based on your reference id
 	  *
 	  * @param memberReferenceId The id used to identify this member in the sending system
 	  * @return The id used in the Ziqni system or None if the user does not exist
 	  */
-	@Deprecated(since = "v1.0.6")
-	def memberIdFromMemberRefId(memberReferenceId: String): Option[String]
+	def memberIdFromMemberRefIdAsync(memberReferenceId: String): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Get the member reference id for the member based on Ziqni id
 	  *
 	  * @param memberId The id used to identify this member in the sending system
 	  * @return The id used in the Ziqni system or None if the user does not exist
 	  */
-	@Deprecated(since = "v1.0.6")
-	def memberRefIdFromMemberId(memberId: String): Option[String]
+	def memberRefIdFromMemberIdAsync(memberId: String): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Create a member in the Ziqni system
 	  *
 	  * @param memberReferenceId The id used to identify this member in the sending system
@@ -113,51 +97,41 @@ trait ZiqniApi extends ZiqniApiAsync {
 	  * @param groups            The groups to add this member to
 	  * @return The id used in the Ziqni system
 	  */
-	@Deprecated(since = "v1.0.6")
-	def createMember(memberReferenceId: String, displayName: String, groups: Seq[String], metaData: Option[Map[String, String]] = None): Option[String]
+	def createMemberAsync(memberReferenceId: String, displayName: String, groups: Seq[String], metaData: Option[Map[String, String]] = None): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param clMemberId     CL Member Id
 	  * @param displayName    Display name
 	  * @param groupsToUpdate The groups to add this member to
 	  * @return The id used in the Ziqni system
 	  */
-	@Deprecated(since = "v1.0.6")
-	def updateMember(clMemberId: String, memberReferenceId: Option[String], displayName: Option[String], groupsToUpdate: Option[Array[String]], metaData: Option[Map[String, String]]): Option[String]
+	def updateMemberAsync(clMemberId: String, memberReferenceId: Option[String], displayName: Option[String], groupsToUpdate: Option[Array[String]], metaData: Option[Map[String, String]]): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param memberId Ziqni Reward Id
 	  * @return BasicMemberModel returns a basic member object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getMember(memberId: String): Option[BasicMemberModel]
+	def getMemberAsync(memberId: String): Future[Option[BasicMemberModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Get the Ziqni id for the product based on your reference id
 	  *
 	  * @param productReferenceId The id used to identify this product in the sending system
 	  * @return The id used in the Ziqni system or None if the product does not exist
 	  */
-	@Deprecated(since = "v1.0.6")
-	def productIdFromProductRefId(productReferenceId: String): Option[String]
+	def productIdFromProductRefIdAsync(productReferenceId: String): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Get the product id for the product based on your Ziqni id
 	  *
 	  * @param productId The id used to identify this product in the sending system
 	  * @return The id used in the Ziqni system or None if the product does not exist
 	  */
-	@Deprecated(since = "v1.0.6")
-	def productRefIdFromProductId(productId: String): Option[String]
+	def productRefIdFromProductIdAsync(productId: String): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param productReferenceId      The id used to identify this product in the sending system
 	  * @param displayName             Display name
@@ -166,11 +140,9 @@ trait ZiqniApi extends ZiqniApiAsync {
 	  * @param defaultAdjustmentFactor The default adjustment factor to apply
 	  * @return The id used in the Ziqni system
 	  */
-	@Deprecated(since = "v1.0.6")
-	def createProduct(productReferenceId: String, displayName: String, providers: Seq[String], productType: String, defaultAdjustmentFactor: Double, metaData: Option[Map[String, String]] = None): Option[String]
+	def createProductAsync(productReferenceId: String, displayName: String, providers: Seq[String], productType: String, defaultAdjustmentFactor: Double, metaData: Option[Map[String, String]] = None): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param clProductId             CL Product Id
 	  * @param displayName             Display name
@@ -179,113 +151,80 @@ trait ZiqniApi extends ZiqniApiAsync {
 	  * @param defaultAdjustmentFactor The default adjustment factor to apply
 	  * @return The id used in the Ziqni system
 	  */
-	@Deprecated(since = "v1.0.6")
-	def updateProduct(clProductId: String, productReferenceId: Option[String], displayName: Option[String], providers: Option[Array[String]], productType: Option[String], defaultAdjustmentFactor: Option[Double], metaData: Option[Map[String, String]]): Option[String]
+	def updateProductAsync(clProductId: String, productReferenceId: Option[String], displayName: Option[String], providers: Option[Array[String]], productType: Option[String], defaultAdjustmentFactor: Option[Double], metaData: Option[Map[String, String]]): Future[Option[String]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param productId Ziqni Product Id
 	  * @return BasicProductModel returns a basic product object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getProduct(productId: String): Option[BasicProductModel]
+	def getProductAsync(productId: String): Future[Option[BasicProductModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  * Verify if the event action type exists in your space
 	  *
 	  * @param action The action
 	  * @return True of the action was created
 	  */
-	@Deprecated(since = "v1.0.6")
-	def eventActionExists(action: String): Boolean
+	def eventActionExistsAsync(action: String): Future[Future[Boolean]]
 
 	/** *
-		* DEPRECATED, use the async methods!
 	  * Create the action in your space
 	  *
 	  * @param action True on success false on failure
 	  * @return
 	  */
-	@Deprecated(since = "v1.0.6")
-	def createEventAction(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None): Boolean
+	def createEventActionAsync(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None): Future[Boolean]
 
 	/** *
-		* DEPRECATED, use the async methods!
 	  * Update the action in your space
 	  *
 	  * @param action True on success false on failure
 	  * @return
 	  */
-	@Deprecated(since = "v1.0.6")
-	def updateEventAction(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None, unitOfMeasureType: Option[String] = None): Boolean
+	def updateEventActionAsync(action: String, name: Option[String] = None, metaData: Option[Map[String, String]] = None, unitOfMeasureType: Option[String] = None): Future[Boolean]
 
 	/**
-		* DEPRECATED, use the async methods!
-	  * [PROTOTYPE]
-	  * Get the spot rate for a currency at a point in time from Oanda
-	  *
-	  * @param fromCurrency From currency ISO3 code
-	  * @param toCurrency   To currency ISO3 code
-	  * @return The rate
-	  */
-	@Deprecated(since = "v1.0.6")
-	def spotExchangeRate(fromCurrency: String, toCurrency: String, pointInTime: DateTime): Double
-
-	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param achievementId Ziqni Achievement Id
 	  * @return BasicAchievementModel returns a basic achievement object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getAchievement(achievementId: String): Option[BasicAchievementModel]
+	def getAchievementAsync(achievementId: String): Future[Option[BasicAchievementModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param contestId Ziqni Contest Id
 	  * @return BasicContestModel returns a basic contest object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getContest(contestId: String): Option[BasicContestModel]
+	def getContestAsync(contestId: String): Future[Option[BasicContestModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param rewardId Ziqni Reward Id
 	  * @return BasicRewardModel returns a basic reward object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getReward(rewardId: String): Option[BasicRewardModel]
+	def getRewardAsync(rewardId: String): Future[Option[BasicRewardModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param awardId Ziqni Award Id
 	  * @return BasicAwardModel returns a basic award object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getAward(awardId: String): Option[BasicAwardModel]
+	def getAwardAsync(awardId: String): Future[Option[BasicAwardModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param unitOfMeasureId Ziqni Unit of Measure Id
 	  * @return BasicUnitOfMeasureModel returns a basic unit of measure object
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getUnitOfMeasure(unitOfMeasureId: String): Option[BasicUnitOfMeasureModel]
+	def getUnitOfMeasureAsync(unitOfMeasureId: String): Future[Option[BasicUnitOfMeasureModel]]
 
 	/**
-		* DEPRECATED, use the async methods!
 	  *
 	  * @param unitOfMeasureKey Ziqni UoM key
 	  * @return Double returns a multiplier associated with the UoM
 	  */
-	@Deprecated(since = "v1.0.6")
-	def getUoMMultiplierFromKey(unitOfMeasureKey: String): Option[Double]
+	def getUoMMultiplierFromKeyAsync(unitOfMeasureKey: String): Future[Option[Double]]
 
 	/**
 	  * Converts a json string to a JValue

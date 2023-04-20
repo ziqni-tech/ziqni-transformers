51fc9a592c9dbafd6cf64ab5b427582e0cec753c
vossie <carel.vosloo@bronzegate.com>
2023-04-20 17:05:57 +0100

    Improved custom fields for developer convenience

--- src/main/scala/com/ziqni/transformers/domain/CustomFieldEntry.scala
+++ src/main/scala/com/ziqni/transformers/domain/CustomFieldEntry.scala
@@ -1,5 +1,7 @@
 package com.ziqni.transformers.domain
 
+import scala.language.implicitConversions
+
 abstract class CustomFieldEntry[T<:Any] (fieldType: String, value: T)
 final case class CustomFieldEntryText(text: String) extends CustomFieldEntry[String](fieldType = "Text", value = text)
 final case class CustomFieldEntryTextArray(texts: Seq[String]) extends CustomFieldEntry[Seq[String]](fieldType = "TextArray", value = texts)
@@ -8,15 +10,15 @@ final case class CustomFieldEntryNumberArray(numbers: Seq[Double]) extends Custo
 
 object CustomFieldEntry {
 
-  private implicit def toText(v: String): CustomFieldEntry[_ <: Any] = CustomFieldEntryText(v)
-  private implicit def toTextArr(v: Seq[String]): CustomFieldEntry[_ <: Any] = CustomFieldEntryTextArray(v)
+  implicit def toText(v: String): CustomFieldEntry[_ <: Any] = CustomFieldEntryText(v)
+  implicit def toTextArr(v: Seq[String]): CustomFieldEntry[_ <: Any] = CustomFieldEntryTextArray(v)
 
-  private implicit def toNumInt(v: Int): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
-  private implicit def toNumIntArr(v: Seq[Int]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v.map(i=>i.doubleValue()))
+  implicit def toNumInt(v: Int): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
+  implicit def toNumIntArr(v: Seq[Int]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v.map(i=>i.doubleValue()))
 
-  private implicit def toNumDouble(v: Double): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
-  private implicit def toNumDoubleArr(v: Seq[Double]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v)
+  implicit def toNumDouble(v: Double): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
+  implicit def toNumDoubleArr(v: Seq[Double]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v)
 
-  private implicit def toNumLong(v: Long): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
-  private implicit def toNumLongArr(v: Seq[Long]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v.map(i=>i.longValue()))
+  implicit def toNumLong(v: Long): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumber(v)
+  implicit def toNumLongArr(v: Seq[Long]): CustomFieldEntry[_ <: Any] = CustomFieldEntryNumberArray(v.map(i=>i.longValue()))
 }

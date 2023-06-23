37ca7fecd88019f318f9e4218659431faad59d25
vossie <carel.vosloo@bronzegate.com>
2023-06-23 16:13:59 +0100

    Adding convenience method for frequently used functions

--- src/main/scala/com/ziqni/transformers/ZiqniApiAsync.scala
+++ src/main/scala/com/ziqni/transformers/ZiqniApiAsync.scala
@@ -91,7 +91,7 @@ trait ZiqniApiAsync {
 		* @param createAs      The object to use when creating the product
 		* @return The id used in the Ziqni system
 		*/
-	def getAndUpdateOrCreateMember(referenceId: String, createAs: () => CreateMemberRequest, onExist: ZiqniMember => ZiqniMember = x=>x): Future[ZiqniMember]
+	def getAndOnExitsOrCreateMember(referenceId: String, createAs: () => CreateMemberRequest, onExist: ZiqniMember => ZiqniMember = x=>x): Future[ZiqniMember]
 
 	/**
 	  *
@@ -149,7 +149,7 @@ trait ZiqniApiAsync {
 	  * @param createAs The object to use when creating the product
 	  * @return The id used in the Ziqni system
 	  */
-	def getAndUpdateOrCreateProduct(referenceId: String, createAs: () => CreateProductRequest, onExist: ZiqniProduct => ZiqniProduct = x=>x): Future[ZiqniProduct]
+	def getAndOnExitsOrCreateProduct(referenceId: String, createAs: () => CreateProductRequest, onExist: ZiqniProduct => ZiqniProduct = x=>x): Future[ZiqniProduct]
 
 	/**
 	  *

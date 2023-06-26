6bada5811f41abd8361efffe9d7fd80594b5f9dc
vossie <carel.vosloo@bronzegate.com>
2023-06-26 14:27:05 +0100

    Removed redundant options

---
+++ src/main/scala/com/ziqni/transformers/ZiqniNotFoundException.scala
@@ -0,0 +1,11 @@
+/***
+  *  Copyright (C) Ziqni Ltd - All Rights Reserved
+  *  Unauthorized copying of this file, via any medium is strictly prohibited
+  *  Proprietary and confidential
+  *  Written by Ziqni Ltd, 2023
+  */
+package com.ziqni.transformers
+
+case class ZiqniNotFoundException(val message: String) extends Exception(message) {
+  override def toString: EventbusAddress = message
+}

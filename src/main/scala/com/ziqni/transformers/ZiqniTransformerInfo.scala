f8458bfca8e8a2e80f050912546e1933cf299bff
vossie <carel.vosloo@bronzegate.com>
2024-04-24 10:25:42 +0100

    Added batched event handler that process the events in a single threaded executor so that ships in the night can be avoided Added a scheduled stub to handle scheduled tick events

--- src/main/scala/com/ziqni/transformers/ZiqniTransformerInfo.scala
+++ src/main/scala/com/ziqni/transformers/ZiqniTransformerInfo.scala
@@ -1,3 +1,3 @@
 package com.ziqni.transformers
 
-case class ZiqniTransformerInfo(connectionId: String, connectionName: String, transformerId: String, customFields: Map[String, List[Object]], user: Option[String], secret: Option[String])
+case class ZiqniTransformerInfo(connectionId: String, connectionName: String, transformerId: String, args: Map[String, List[Object]])

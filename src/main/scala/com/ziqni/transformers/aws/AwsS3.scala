00701fe3506b2f7e762599147bcb3423afe88dca
vossie <carel.vosloo@bronzegate.com>
2024-06-19 19:03:13 +0100

    Adding support for alt communication

--- src/main/scala/com/ziqni/transformers/aws/AwsS3.scala
+++ src/main/scala/com/ziqni/transformers/aws/AwsS3.scala
@@ -1,36 +1,19 @@
 package com.ziqni.transformers.aws
 
-import software.amazon.awssdk.core.ResponseInputStream
-import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.{DeleteObjectRequest, DeleteObjectResponse, GetObjectRequest, GetObjectResponse, ListObjectsV2Request, S3Object}
-import software.amazon.awssdk.regions.Region
-
-import scala.collection.mutable
-import scala.jdk.CollectionConverters._
+import awscala._
+import awscala.s3._
+import com.amazonaws.services.s3.model.{S3Object, S3ObjectSummary}
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 case class AwsS3(ziqniAwsCredentials: AwsCredentials, bucketName: String, region: String) {
 
-  private val s3Client = S3Client.builder()
-    .region(Region.of(region))
-    .credentialsProvider(ziqniAwsCredentials.awsStaticCredentialsProvider)
-    .build()
+  // Implicit region declaration
+  implicit val regionImpl: Region = Region.apply(region)
+  private val s3Client: S3 = S3.apply(ziqniAwsCredentials.accessKeyId, ziqniAwsCredentials.secretAccessKey)(regionImpl)
 
-  def close(): Unit = {
-    try {
-      s3Client.close()
-    }
-    catch {
-      case e: Exception => e.printStackTrace()
-    }
-  }
-
-  def listObjects(f :mutable.Seq[S3Object] => Unit): Unit  = {
-    val listObjectsRequest = ListObjectsV2Request.builder()
-      .bucket(bucketName)
-      .build()
-
-    val listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest)
-    val seq = listObjectsResponse.contents().asScala
+  def listObjects(f :Iterable[S3ObjectSummary] => Unit): Unit  = {
+    val listObjectsResponse = s3Client.listObjectsV2(bucketName)
+    val seq = listObjectsResponse.getObjectSummaries.asScala
     f.apply(seq)
   }
 
@@ -39,40 +22,25 @@ case class AwsS3(ziqniAwsCredentials: AwsCredentials, bucketName: String, region
    * @param prefix Like folderA or FolderA/folderB
    * @return
    */
-  def listObjectSubFolders(prefix:String, f :mutable.Seq[S3Object] => Unit): Unit  = {
+  def listObjectSubFolders(prefix:String, f :Iterable[S3ObjectSummary] => Unit): Unit  = {
     val p = {
       if(prefix.endsWith("/")) prefix
       else prefix+"/"
     }
-    val listObjectsRequest = ListObjectsV2Request.builder()
-      .bucket(bucketName)
-      .prefix(p)
-      .build()
 
-    val listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest)
-    val seq = listObjectsResponse.contents().asScala
+    val listObjectsResponse = s3Client.listObjectsV2(bucketName,p)
+    val seq = listObjectsResponse.getObjectSummaries.asScala
     f.apply(seq)
   }
 
   // Get an object from the bucket
-  def getObject(key: String, f :ResponseInputStream[GetObjectResponse] => Unit): Unit  = {
-    val getObjectRequest = GetObjectRequest.builder()
-      .bucket(bucketName)
-      .key(key)
-      .build()
-
-    val s = s3Client.getObject(getObjectRequest)
+  def getObject(key: String, f : S3Object => Unit): Unit  = {
+    val s = s3Client.getObject(bucketName, key)
     f.apply(s)
   }
 
   // Delete an object from the bucket
-  def deleteObject(key: String, f :DeleteObjectResponse => Unit): Unit  = {
-    val deleteObjectRequest = DeleteObjectRequest.builder()
-      .bucket(bucketName)
-      .key(key)
-      .build()
-
-    val r = s3Client.deleteObject(deleteObjectRequest)
-    f.apply(r)
+  def deleteObject(key: String): Unit  = {
+    s3Client.deleteObject(bucketName, key)
   }
 }
